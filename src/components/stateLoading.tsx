fc5b98ed5ff00b4e45615908620bd61524988434
Lando Winzen <winzenlando@gmail.com>
2025-10-14 07:30:38 +0000

    Loadingscreen rework

--- src/components/stateLoading.tsx
+++ src/components/stateLoading.tsx
@@ -1,6 +1,11 @@
 export const StateLoading = () => {
     return (
         <div className='flex flex-col items-center justify-center bg-linear-to-r from-blue-200 to-blue-400 h-screen text-center'>
+
+            <div className="flex items-center w-[225px] h-[48px] bg-gray-100/30 mb-5 rounded-[20px] p-3">
+                <div className="w-[180px] h-[24px] animate-pulse bg-gray-300/40 m-2 rounded-[10px]"></div>
+            </div>
+
             <div className='flex flex-col items-center w-[290px] h-[104px] text-shadow-md bg-gray-100/30 rounded-[20px]'>
                 <div className="flex flex-col items-center w-[290px] h-[104px] animate-pulse">
                     <div className='w-[200px] animate-pulse h-[28px] bg-gray-300/40 rounded-[10px] m-2'>
@@ -25,6 +30,22 @@ export const StateLoading = () => {
                     <div className=" w-[50px] h-[72px] bg-gray-300/40 rounded-[10px]"></div>
                 </div>
             </div>
+
+            <div className="flex flex-col w-[500px] h-[320px] bg-gray-100/30 rounded-[20px]">
+                <div className="flex items-center animate-pulse flex-col w-[500px] h-[320px]">
+                    <div className="w-[460px] h-[38px] bg-gray-300/40 rounded-[10px] mt-2"></div>
+                    <div className="w-[460px] h-[38px] bg-gray-300/40 rounded-[10px] mt-2"></div>
+                    <div className="w-[460px] h-[38px] bg-gray-300/40 rounded-[10px] mt-2"></div>
+                    <div className="w-[460px] h-[38px] bg-gray-300/40 rounded-[10px] mt-2"></div>
+                    <div className="w-[460px] h-[38px] bg-gray-300/40 rounded-[10px] mt-2"></div>
+                    <div className="w-[460px] h-[38px] bg-gray-300/40 rounded-[10px] mt-2"></div>
+                    <div className="w-[460px] h-[38px] bg-gray-300/40 rounded-[10px] mt-2"></div>
+                </div>
+            </div>
         </div >
     )
 }
+
+//225 x 48
+//320
+//460 x 40
